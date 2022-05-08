a7a687813b06ff2e4f6ae176aa2a12f645484d19
Yulian <yulianguinand365@gmail.com>
2022-05-08 11:37:09 +0200

    contact

--- form-handler.php
+++ form-handler.php
@@ -1,26 +1,47 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+require_once(__DIR__ . '/vendor/autoload.php');
+use \Mailjet\Resources;
+define('API_PUBLIC_KEY', 'YOUR_KEY');
+define('API_PRIVATE_KEY', 'YOUR_KEY');
+$mj = new \Mailjet\Client(API_PUBLIC_KEY, API_PRIVATE_KEY,true,['version' => 'v3.1']);
 
-$email_from = 'contact-Portfolio@portfolio.com';
 
-$email_subject = 'New Form Submission';
+if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['message'])){
+    $surname = htmlspecialchars($_POST['surname']);
+    $firstname = htmlspecialchars($_POST['firstname']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
 
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                    "Subject: $subject.\n".
-                    "User message: $message.\n";
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+    $body = [
+        'Messages' => [
+        [
+            'From' => [
+            'Email' => "EMAIL",
+            'Name' => "NAME"
+            ],
+            'To' => [
+            [
+                'Email' => "EMAIL",
+                'Name' => "NAME"
+            ]
+            ],
+            'Subject' => "SUJECT",
+            'TextPart' => 'TEXT EMAIL', 
+            'HTMLPart' => "TEXT EMAIL",
+            'CustomID' => "AppGettingStartedTest"
+        ]
+        ]
+    ];
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+        $response->success();
+        echo "Email envoyé avec succès !";
+    }
+    else{
+        echo "Email non valide";
+    }
 
-$to = 'yulianguinand@orange.com';
-
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: index.html");
-
-?>
+} else {
+    header('Location: index.php');
+    die();
+}
